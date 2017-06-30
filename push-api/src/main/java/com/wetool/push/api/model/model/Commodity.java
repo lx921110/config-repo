ec365a2b0e23838c783dbb6d2be8aa076392ffb7
zhangjie <zhangjie@weitoo.com>
2017-06-30 21:17:19 +0800

    UP

---
+++ push-api/src/main/java/com/wetool/push/api/model/model/Commodity.java
@@ -0,0 +1,8 @@
+package com.wetool.push.api.model.model;
+
+import java.io.Serializable;
+
+public class Commodity implements Serializable {
+	private static final long serialVersionUID = 7165007362209942970L;
+
+}
--- push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
+++ push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
@@ -1,17 +1,23 @@
 package com.wetool.push.api.model.server;
 
+import java.util.List;
 import com.wetool.push.api.model.MsgType;
 import com.wetool.push.api.model.Result;
 import com.wetool.push.api.model.S2ClientResp;
+import com.wetool.push.api.model.model.Commodity;
+
 import lombok.Data;
 
 @Data
-public class CommodityResp extends S2ClientResp{
+public class CommodityResp extends S2ClientResp {
 	private static final long serialVersionUID = -3828291867249691076L;
 	
 	public CommodityResp(MsgType type, Result result) {
 		super(type, result);
 	}
 	
-	
+	int number;	//当前页
+	int totalPages;	//总页数
+
+	public List<Commodity> commoditys;
 }
