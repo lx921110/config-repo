030d873ba7ef8bc27a34ca85800bc7b6ad643b94
zhanbin <zhanbin@weitoo.com>
2017-07-05 09:33:59 +0800

    修改部分push-api注释。 修改gradle版本为3.4.1

--- push-api/src/main/java/com/wetool/push/api/model/MsgType.java
+++ push-api/src/main/java/com/wetool/push/api/model/MsgType.java
@@ -2,41 +2,70 @@ package com.wetool.push.api.model;
 
 /**
  * 消息类型
+ *
  * @author zhangjie
  */
-public enum  MsgType {
-	
-	/** ============消息类型（客户端 -> 服务端） */
-	/** 登录请求 */
-	LOGIN_REQ,
-	
-	/** Ping请求 */
+public enum MsgType {
+
+    /** ============消息类型（客户端 -> 服务端） */
+    /**
+     * 登录请求
+     * Client - > Server
+     */
+    LOGIN_REQ,
+
+    /**
+     * Ping请求
+     * Client - > Server
+     */
     PING_REQ,
-    
-    /** 版本同步请求 */
-    VERSION_REQ	,
-    
-    /** 商品信息同步请求 */
-    COMMODITY_REQ ,
-    
+
+    /**
+     * 版本同步请求
+     * Client - > Server
+     */
+    VERSION_REQ,
+
+    /**
+     * 商品信息同步请求
+     * Client - > Server
+     */
+    COMMODITY_REQ,
+
     /** ============消息类型（服务端 -> 客户端） */
-	/** 登录响应 */
+    /**
+     * 登录响应
+     * Server - > Client
+     */
     LOGIN_RESP,
-    
-    /** 重新登录请求 */
-	RELOGIN_REQ,
+
+    /**
+     * 重新登录请求
+     * Server - > Client
+     */
+    RELOGIN_REQ,
 
     /**
      * 推送服务
+     * Server - > Client
      */
     PUSH_REQ,
-	
-	 /** ping响应 */
-	PING_RESP,
-	
-	 /** 商品信息同步响应 */
-    COMMODITY_RESP ,
-    
-    /** 版本同步响应 */
+
+    /**
+     * ping响应
+     * Server - > Client
+     */
+    PING_RESP,
+
+    /**
+     * 商品信息同步响应
+     * Server - > Client
+     */
+    COMMODITY_RESP,
+
+    /**
+     * 版本同步响应
+     * Server - > Client
+     */
     VERSION_RESP
 }
