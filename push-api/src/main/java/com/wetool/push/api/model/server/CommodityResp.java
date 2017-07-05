dadc75b944817dc04736468b86d9aa043812f194
wangweifeng <wangweifeng@wetool.com>
2017-07-05 21:36:47 +0800

    up

--- push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
+++ push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
@@ -1,16 +1,22 @@
 package com.wetool.push.api.model.server;
 
-import java.io.Serializable;
 import java.util.List;
+import com.wetool.push.api.model.BaseMessage;
+import com.wetool.push.api.model.MsgType;
 import com.wetool.push.api.model.model.Commodity;
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.Data;
+@Getter
+@Setter
+public class CommodityResp extends BaseMessage {
+    private static final long serialVersionUID = -3828291867249691076L;
 
-@Data
-public class CommodityResp implements Serializable {
-	private static final long serialVersionUID = -3828291867249691076L;
-		
-	private Boolean flag; //检索是否还有内容
+    private Boolean flag; //检索是否还有内容
 
-	public List<Commodity> commoditys; //商品信息集合
+    public List<Commodity> commoditys; //商品信息集合
+
+    public CommodityResp() {
+        super(MsgType.COMMODITY_RESP);
+    }
 }
