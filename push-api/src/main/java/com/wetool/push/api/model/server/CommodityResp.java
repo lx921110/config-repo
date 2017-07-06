64c9175df2b6515d95956ed67fcac444ca22ad37
lixin <lixin@wetool.com>
2017-07-06 18:03:48 +0800

    UP

--- push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
+++ push-api/src/main/java/com/wetool/push/api/model/server/CommodityResp.java
@@ -1,7 +1,11 @@
 package com.wetool.push.api.model.server;
 
+import java.util.List;
+
 import com.wetool.push.api.model.BaseMessage;
 import com.wetool.push.api.model.MsgType;
+import com.wetool.push.api.model.model.Commodity;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +15,7 @@ public class CommodityResp extends BaseMessage {
 
     private Boolean flag; //检索是否还有内容
 
-    //public List<Commodity> commoditys; //商品信息集合
+    public List<Commodity> commoditys; //商品信息集合
 
     public CommodityResp() {
         super(MsgType.COMMODITY_RESP);
