755569558d62573add7cef9da19d07f88ed29fed
zhanbin <zhanbin@weitoo.com>
2017-07-14 10:53:53 +0800

    取消NettyServerHandler的错误注释

--- push-server/src/main/java/com/wetool/push/server/NettyServerHandler.java
+++ push-server/src/main/java/com/wetool/push/server/NettyServerHandler.java
@@ -12,8 +12,6 @@ import com.wetool.push.server.service.CategoryService;
 import com.wetool.push.server.service.CommodityService;
 import com.wetool.push.server.service.LoginService;
 import com.wetool.push.server.service.VersionService;
-
-import ch.qos.logback.core.net.SyslogOutputStream;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -25,101 +23,112 @@ import org.springframework.stereotype.Component;
 @Component
 @Sharable
 public class NettyServerHandler extends SimpleChannelInboundHandler<C2ServerReq> {
-	
-	@Autowired
-	LoginService loginService;
-	
-	@Autowired
-	VersionService versionService;
-	
-	@Autowired
-	CommodityService commodityService;
-	
-	@Autowired
-	CategoryService categoryService;
-	
-	/** 通道失效处理 */
-	@Override
-	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		/* 当通道失效时，从通道列表中移除 */
-		NettyChannelMap.remove((SocketChannel) ctx.channel());
-	}
 
-	/** 消息接收 */
+    @Autowired
+    LoginService loginService;
+
+    @Autowired
+    VersionService versionService;
+
+    @Autowired
+    CommodityService commodityService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    /**
+     * 通道失效处理
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        /* 当通道失效时，从通道列表中移除 */
+        NettyChannelMap.remove((SocketChannel) ctx.channel());
+    }
+
+    /**
+     * 消息接收
+     */
 //	@Override
 //	protected void messageReceived(ChannelHandlerContext channelHandlerContext, C2ServerReq c2ServerReq) throws Exception {
 //
 //	}
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, C2ServerReq c2ServerReq) throws Exception {
+        // TODO Auto-generated method stub
+        System.out.println(c2ServerReq.getType().name());
 
-	@Override
-	protected void channelRead0(ChannelHandlerContext channelHandlerContext, C2ServerReq c2ServerReq) throws Exception {
-		// TODO Auto-generated method stub
+        if (MsgType.LOGIN_REQ.equals(c2ServerReq.getType())) {
+            LoginReq loginMsg = (LoginReq) c2ServerReq;
+            String clientId = loginMsg.getClientId();    //客户端ID（设备SN）
+            if (clientId == null) {
+                System.out.println("clientId == null");
+                return;
+            }
+            String token = loginMsg.getToken();    // 令牌
+            ContextHolder.set(token);
 
-		if (MsgType.LOGIN_REQ.equals(c2ServerReq.getType())) {
-			LoginReq loginMsg = (LoginReq) c2ServerReq;
-			String clientId = loginMsg.getClientId();	//客户端ID（设备SN）
-			String token = loginMsg.getToken();	// 令牌
-			ContextHolder.set(token);
-			
-			System.out.println("客户端[" + loginMsg.getClientId() + "] token=" + token);
-			S2ClientResp resp = loginService.login(clientId);
-			if (Result.SUCCESS == resp.getResult()) {	// 如果登录成功
-				// 当前客户端通道加入通道集合
-				NettyChannelMap.add(clientId, (SocketChannel) channelHandlerContext.channel());
-			}
-			/* 返回响应消息 */
-			NettyChannelMap.get(loginMsg.getClientId()).writeAndFlush(resp);
-		} else {
-			/* 未登录或连接中断，服务器要求客户端重新登录 */
-			if (NettyChannelMap.get(c2ServerReq.getClientId()) == null) {
-				ReloginReq reloginReq = new ReloginReq();
-				channelHandlerContext.channel().writeAndFlush(reloginReq);
-			}
-		}
-		
-//		switch (c2ServerReq.getType()) {
-//		case PING_REQ: {
-//			PingReq pingReq = (PingReq) c2ServerReq;
-//			S2ClientResp s2ClientResp = new S2ClientResp(MsgType.PING_RESP, Result.SUCCESS);
-//			System.out.println(pingReq.getClientId());
-//			NettyChannelMap.get(pingReq.getClientId()).writeAndFlush(s2ClientResp);
-//		}
-//			break;
-//		case VERSION_REQ: {	// 版本同步请求
-//			VersionReq versionReq = (VersionReq) c2ServerReq;
-//			S2ClientResp s2ClientResp = versionService.save(versionReq);
-//			NettyChannelMap.get(versionReq.getClientId()).writeAndFlush(s2ClientResp);
-//		}
-//			break;
-//		case COMMODITY_REQ :{// 商品信息同步请求 
-//			CommodityReq commodityReq = (CommodityReq) c2ServerReq;
-//			CommodityResp s2ClientResp = commodityService.commSync(commodityReq);
-//			System.out.println("消息——————》" + "发送成功 ！ " + s2ClientResp.toString());
-//			System.out.println(commodityReq.getClientId());
-////			S2ClientResp  s2ClientResp = new S2ClientResp<>(MsgType.COMMODITY_RESP, Result.SUCCESS);
-//			try{
-//				NettyChannelMap.get(commodityReq.getClientId()).writeAndFlush(s2ClientResp);
-//			}catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		  break;
-//		case CATEGORY_REQ:{
-//			CategoryReq categoryReq = (CategoryReq)c2ServerReq;
-//			CategoryResp categoryResp = categoryService.categorySync(categoryReq);
-//			NettyChannelMap.get(categoryReq.getClientId()).writeAndFlush(categoryResp);
-//		}
-//		break;
-//		case SimpleTest_REQ: 
-//			SimpleTest test = (SimpleTest)c2ServerReq;
-//			System.out.println(test.getHello());
-//			break;
-//		default:
-//			break;
-//		}
-		ReferenceCountUtil.release(c2ServerReq);
-	}
+            System.out.println("客户端[" + loginMsg.getClientId() + "] token=" + token);
+            S2ClientResp resp = loginService.login(clientId);
+            if (resp == null) {
+                System.out.println("resp == null");
+                return;
+            }
+            if (Result.SUCCESS == resp.getResult()) {    // 如果登录成功
+                // 当前客户端通道加入通道集合
+                NettyChannelMap.add(clientId, (SocketChannel) channelHandlerContext.channel());
+            }
+            /* 返回响应消息 */
+            NettyChannelMap.get(clientId).writeAndFlush(resp);
+        } else {
+            /* 未登录或连接中断，服务器要求客户端重新登录 */
+            if (NettyChannelMap.get(c2ServerReq.getClientId()) == null) {
+                ReloginReq reloginReq = new ReloginReq();
+                channelHandlerContext.channel().writeAndFlush(reloginReq);
+            }
+        }
 
+        switch (c2ServerReq.getType()) {
+            case PING_REQ: {
+                PingReq pingReq = (PingReq) c2ServerReq;
+                S2ClientResp s2ClientResp = new S2ClientResp(MsgType.PING_RESP, Result.SUCCESS);
+                System.out.println(pingReq.getClientId());
+                NettyChannelMap.get(pingReq.getClientId()).writeAndFlush(s2ClientResp);
+            }
+            break;
+            case VERSION_REQ: {    // 版本同步请求
+                VersionReq versionReq = (VersionReq) c2ServerReq;
+                S2ClientResp s2ClientResp = versionService.save(versionReq);
+                NettyChannelMap.get(versionReq.getClientId()).writeAndFlush(s2ClientResp);
+            }
+            break;
+            case COMMODITY_REQ: {// 商品信息同步请求
+                CommodityReq commodityReq = (CommodityReq) c2ServerReq;
+                CommodityResp s2ClientResp = commodityService.commSync(commodityReq);
+                System.out.println("消息——————》" + "发送成功 ！ " + s2ClientResp.toString());
+                System.out.println(commodityReq.getClientId());
+//			S2ClientResp  s2ClientResp = new S2ClientResp<>(MsgType.COMMODITY_RESP, Result.SUCCESS);
+                try {
+                    NettyChannelMap.get(commodityReq.getClientId()).writeAndFlush(s2ClientResp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            break;
+            case CATEGORY_REQ: { //商品分类
+                CategoryReq categoryReq = (CategoryReq) c2ServerReq;
+                CategoryResp categoryResp = categoryService.categorySync(categoryReq);
+                System.out.println("消息——————》" + "发送成功 ！ " + categoryReq.getType().name());
+                System.out.println(categoryReq.getClientId());
+                NettyChannelMap.get(categoryReq.getClientId()).writeAndFlush(categoryResp);
+            }
+            break;
+
+            default:
+                System.out.println(c2ServerReq.getType().name());
+                break;
+        }
+        ReferenceCountUtil.release(c2ServerReq);
+    }
 
 
 }
--- push-server/src/main/java/com/wetool/push/server/service/CategoryService.java
+++ push-server/src/main/java/com/wetool/push/server/service/CategoryService.java
@@ -1,18 +1,16 @@
 package com.wetool.push.server.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import com.wetool.push.api.model.client.CategoryReq;
 import com.wetool.push.api.model.model.CategoryTree;
 import com.wetool.push.api.model.server.CategoryResp;
 import com.wetool.push.server.feign.CommodityFeignClient;
 import com.wetool.push.server.model.Message;
-
 import feign.Feign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class CategoryService {
@@ -24,16 +22,16 @@ public class CategoryService {
 	private String url;
 	
 	public CategoryResp categorySync(CategoryReq categoryReq){
-		url = "http://192.168.1.91:16010";
+//		url = "http://192.168.1.91:16010";
 		/* 商品获取接口 */
 		CommodityFeignClient commodityFeignClient = builder.target(CommodityFeignClient.class, url);
 		ResponseEntity<Message<ArrayList<CategoryTree>>> res = commodityFeignClient.categorylist(categoryReq.getMerchantId());
-		Message message = res.getBody();
+		Message<ArrayList<CategoryTree>> message = res.getBody();
 //		List<CategoryTree> cate = (List<CategoryTree>) message.getData();
 //		System.out.println(cate.size());
 		//return new S2ClientResp<>(MessageType., result);
 		ArrayList<CategoryTree> list = new ArrayList<CategoryTree>();
-		ArrayList<CategoryTree> ca = (ArrayList<CategoryTree>) message.getData();
+		ArrayList<CategoryTree> ca = message.getData();
 		list.addAll(ca);
 		return new CategoryResp(list);
 	}
